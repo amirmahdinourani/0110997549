08574e2cb64016caf3ad376fa75bf36edd0f7aeb
amirmahdinourani <166804536+amirmahdinourani@users.noreply.github.com>
2024-07-01 18:09:36 +0330

    Upload simple PWM project on Githup

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+darkMode:'media',
+  theme: {
+    fontSize:{
+      'font1':'20px',
+    },
+    colors:{
+      'rang':'pink',
+    },
+    extend: {
+   
+    },
+    
+   
+  },
+
+  plugins: [],
+}
+
